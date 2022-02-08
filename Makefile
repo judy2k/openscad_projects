520f4918c8172f51e21047d98ead53a0ad559b01
Judy2k <judy@judy.co.uk>
2022-02-08 10:32:03 +0000

    Regenerate previews with different aspect ratio.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SUBPROJECTS := blobby cable_clip diamond fidget_cog_toy flower_pot_tray wart_sphere
 
 all:
-	for d in $(SUBPROJECTS); do echo $(MAKE) -C $${d} all; done
+	for d in $(SUBPROJECTS); do $(MAKE) -C $${d} all; done
 
 clean:
 	for d in $(SUBPROJECTS); do $(MAKE) -C $${d} clean; done
--- patterns.mk
+++ patterns.mk
@@ -2,10 +2,10 @@ SCAD = OpenSCAD
 STLFLAGS = --export-format binstl
 
 %_prepared.png: %.scad
-	$(SCAD) -o $@ -D 'quick=false' -D 'prepare=true' $(PARAMFLAGS) $<
+	$(SCAD) -o $@ --render --imgsize=1280,720 -D 'quick=false' -D 'prepare=true' $(PARAMFLAGS) $<
 
 %.png: %.scad
-	$(SCAD) -o $@ -D 'quick=false' -D 'prepare=false' $(PARAMFLAGS) $<
+	$(SCAD) -o $@ --render --imgsize=1280,720 -D 'quick=false' -D 'prepare=false' $(PARAMFLAGS) $<
 
 %_prepared.stl: %.scad
 	$(SCAD) $(STLFLAGS) -o $@ -D 'quick=false' -D 'prepare=true' $(PARAMFLAGS) $<

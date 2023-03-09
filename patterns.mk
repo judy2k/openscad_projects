eb9a9b9812b8fd50bfe86b80ac23cf2e4323a173
Judy2k <judy@judy.co.uk>
2023-03-09 16:12:02 +0000

    Render in the default color-scheme.

--- patterns.mk
+++ patterns.mk
@@ -5,7 +5,7 @@ STLFLAGS = --export-format binstl
 	$(SCAD) -o $@ --render --imgsize=1280,720 -D 'quick=false' -D 'prepare=true' $(PARAMFLAGS) $<
 
 %.png: %.scad
-	$(SCAD) -o $@ --colorscheme=BeforeDawn --render --imgsize=1280,720 -D 'quick=false' -D 'prepare=false' $(PARAMFLAGS) $<
+	$(SCAD) -o $@ --render --imgsize=1280,720 -D 'quick=false' -D 'prepare=false' $(PARAMFLAGS) $<
 
 %_prepared.stl: %.scad
 	$(SCAD) $(STLFLAGS) -o $@ -D 'quick=false' -D 'prepare=true' $(PARAMFLAGS) $<
